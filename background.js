35133ec46d29d052bb6d804006975c2e788b3b2f
linghong <linghong_chen@hotmail.com>
2018-04-25 12:44:11 -0400

    added green button.

--- background.js
+++ background.js
@@ -2,12 +2,13 @@ chrome.runtime.onInstalled.addListener(()=>{
 	chrome.storage.sync.set({color:'#3aa757'}, ()=>{
 		console.log("The color is green.");
 	});
-	chrome.declaractiveContent.onPageChanged.removeRules(undefined, ()=>{
+	chrome.declarativeContent.onPageChanged.removeRules(undefined, ()=>{
 		chrome.declarativeContent.onPageChanged.addRules([{
-			conditiond: [new chrome.declarativeContent.PageStateMatcher({
-				pageUrl: {hostEquals: 'developer.chrome.com'},
-			})],
-			actions: [new chrome.declarativeContent.ShowPageAction()]
+			conditions: [new chrome.declarativeContent.PageStateMatcher({
+          pageUrl: {hostEquals: 'developer.chrome.com'},
+        })
+        ],
+            actions: [new chrome.declarativeContent.ShowPageAction()]
 		}]);
 	});
 });
