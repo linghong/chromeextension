6d39829ea22e7d5b7a5585f576d64f67d4a5e5b4
linghong <linghong_chen@hotmail.com>
2018-04-25 12:13:31 -0400

    now the browser will show a colored icon when navigating to developer.chrome.com.

--- background.js
+++ background.js
@@ -1,5 +1,13 @@
 chrome.runtime.onInstalled.addListener(()=>{
 	chrome.storage.sync.set({color:'#3aa757'}, ()=>{
 		console.log("The color is green.");
-	}
-}
+	});
+	chrome.declaractiveContent.onPageChanged.removeRules(undefined, ()=>{
+		chrome.declarativeContent.onPageChanged.addRules([{
+			conditiond: [new chrome.declarativeContent.PageStateMatcher({
+				pageUrl: {hostEquals: 'developer.chrome.com'},
+			})],
+			actions: [new chrome.declarativeContent.ShowPageAction()]
+		}]);
+	});
+});
